76e34e127651d72814015a8cb0d1cd1c43373af3
bemrung2 <bemrung2@gmail.com>
2021-02-16 16:27:09 +0900

    210216

---
+++ Implementation/ect4_2.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+
+count = 0
+
+for i in range(n+1):
+    for j in range(60):
+        for k in range(60):
+            # 각 시간 단위에 3이 포함되어 있으면 count 증가
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+
+print(count)
