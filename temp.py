fcc2a98dd20111e864b07462b8971a29aa58432b
bemrung2 <bemrung2@gmail.com>
2021-02-19 23:18:50 +0900

    210219

---
+++ temp.py
@@ -0,0 +1,42 @@
+# import sys
+
+# n = int(sys.stdin.readline().rstrip())
+
+# count = 0
+
+# for i in range(n+1):
+#     for j in range(60):
+#         for k in range(60):
+#             # 각 시간 단위에 3이 포함되어 있으면 count 증가
+#             if '3' in str(i) + str(j) + str(k):
+#                 count += 1
+
+print(ord('a'))
+print(chr(97))
+
+
+
+# ################ init 테스트 (계산기)
+# class FourCal:
+#     def __init__(self, first, second):
+#         self.first = first
+#         self.second = second
+#     # def setdata(self, first, second):
+#     #     self.first = first
+#     #     self.second = second
+#     def add(self):
+#         result = self.first + self.second
+#         print(result)
+#     def mul(self):
+#         result = self.first * self.second
+#         return result
+#     def sub(self):
+#         result = self.first - self.second
+#         return result
+#     def div(self):
+#         result = self.first / self.second
+#         return result
+
+# a = FourCal(4,2)
+# a.add()
+
