b9565c3d490a7bfcdaf7414acab7a6a800ed034b
bemrung2 <bemrung2@gmail.com>
2021-03-11 21:08:28 +0900

    210311

--- DFS,BFS/baekjoon_1260.py
+++ DFS,BFS/baekjoon_1260.py
@@ -2,37 +2,72 @@ from collections import deque
 import sys
 
 n, m, v = map(int, sys.stdin.readline().rstrip().split())
-matrix = [[0] * (n+1) for i in range(n+1)]
-for i in range(m):
+
+# graph = [list(map(int, list(sys.stdin.readline().rstrip().split()))) for _ in range(m)]
+graph = [[0] * (n+1) for _ in range(n+1)] # 한 칸씩 추가로 생성 (인덱스를 그대로 사용하기 위함) 
+for _ in range(m):
     x, y = map(int, sys.stdin.readline().rstrip().split())
-    matrix[x][y] = matrix[y][x] = 1
-
-
+    graph[x][y] = graph[y][x] = 1
 
 visited = [False] * (n+1)
-
 def dfs(v):
     visited[v] = True
-    print(v, end= ' ')
+    print(v, end = ' ')
     for i in range(1, n+1):
-        if visited[i] == False and matrix[v][i] == 1:
+        if (visited[i] == False) and (graph[v][i] == 1):
             dfs(i)
 
-from collections import deque
+dfs(v)
+
 def bfs(v):
     queue = deque([v])
-    visited[v] = False
+    visited[v] = False  # 앞에서 True로 했기 때문에 bfs에서는 반대로 진행한다.
     while queue:
         v = queue.popleft()
         print(v, end = ' ')
         for i in range(1, n+1):
-            if visited[i] == True and matrix[v][i] ==1:
+            if visited[i] == True and graph[v][i] ==1:
                 queue.append(i)
                 visited[i] = False
 
-dfs(v)
 print()
 bfs(v)
+
+
+# from collections import deque
+# import sys
+
+# n, m, v = map(int, sys.stdin.readline().rstrip().split())
+# matrix = [[0] * (n+1) for i in range(n+1)]
+# for i in range(m):
+#     x, y = map(int, sys.stdin.readline().rstrip().split())
+#     matrix[x][y] = matrix[y][x] = 1
+
+
+# visited = [False] * (n+1)
+
+# def dfs(v):
+#     visited[v] = True
+#     print(v, end= ' ')
+#     for i in range(1, n+1):
+#         if visited[i] == False and matrix[v][i] == 1:
+#             dfs(i)
+
+# from collections import deque
+# def bfs(v):
+#     queue = deque([v])
+#     visited[v] = False
+#     while queue:
+#         v = queue.popleft()
+#         print(v, end = ' ')
+#         for i in range(1, n+1):
+#             if visited[i] == True and matrix[v][i] ==1:
+#                 queue.append(i)
+#                 visited[i] = False
+
+# dfs(v)
+# print()
+# bfs(v)
 
 
 # import sys
