ea8a980f90c7be9e5382ba8d4e039e23816856dc
bemrung2 <bemrung2@gmail.com>
2021-03-29 14:54:23 +0900

    210329

--- DFS,BFS/baekjoon_4963.py
+++ DFS,BFS/baekjoon_4963.py
@@ -1,31 +1,31 @@
 import sys
 sys.setrecursionlimit(10000) # 메모리초과 해결
-input = sys.stdin.readline
 
 dx = [1, -1, 0, 0, 1, -1, -1, 1]
 dy = [0, 0, -1, 1, 1, 1, -1, -1]
 
 
 while True:
-    w, h = map(int,input().split())
+    w, h = map(int, sys.stdin.readline().split())
     if w == 0:
         break
-    island = [list(map(int,input().split())) for _ in range(h)]
-    visited = [[False]*w for _ in range(h)]
+    graph = [list(map(int,sys.stdin.readline().split())) for _ in range(h)]
+    visited = [[0] * w for _ in range(h)]
     
     def dfs(x, y):
-        visited[x][y] = True
+        visited[x][y] = 1
         for i in range(8):
             nx, ny = x+dx[i], y+dy[i]
             if nx<0 or nx>=h or ny<0 or ny>=w:
                 continue
-            if island[nx][ny]== 1 and visited[nx][ny] is False:
+            if graph[nx][ny]== 1 and visited[nx][ny] == 0:
                 dfs(nx,ny)
                 
     cnt = 0
     for i in range(h):
         for j in range(w):
-            if island[i][j] == 1 and visited[i][j] is False:
+            if graph[i][j] == 1 and visited[i][j] == 0:
                 dfs(i,j)
                 cnt += 1
     print(cnt)
+
