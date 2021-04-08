e4c2bf3d06b7cf0d06278cc1d180d4606c5b37ef
bemrung2 <bemrung2@gmail.com>
2021-04-08 22:50:29 +0900

    210408

---
+++ Greedy/baekjoon_11399.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+pi = list(map(int, sys.stdin.readline().split()))
+
+pi.sort()
+result = 0
+
+for i in range(n):
+    for j in range(i+1):
+        result += pi[j]
+
+print(result)
+
