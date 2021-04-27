c943cbe5397d4f84f66bc4e0bd6d954a6f99871e
뱀렁이 <bemrung2@naver.com>
2021-04-27 15:55:42 +0000

    210428

--- Others/baekjoon_1188.py
+++ Others/baekjoon_1188.py
@@ -1,9 +1,12 @@
+# tttest .
+
 import sys
 
 def GCD(a, b):
     if a % b == 0:
         return b
     return GCD(b, a % b)
-n, m = map(int, sys.stdin.readline().rstrip().split())
-print(m - GCD(n, m))
 
+n, m = map(int, sys.stdin.readline().rstrip().split())
+
+print(m - GCD(n, m))
---
+++ Others/goormtest.py
@@ -0,0 +1,1 @@
+print('Hello goorm IDE')
