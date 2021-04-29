902ff938aa98509c90bdbe7bfe7ab8a4ee7a1aa8
bemrung2 <bemrung2@gmail.com>
2021-04-29 18:01:11 +0900

    210429

---
+++ Greedy/baekjoon_2839.py
@@ -0,0 +1,34 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+ans = 0
+
+while n >= 0:
+    if n % 5 == 0:
+        ans += (n // 5)
+        print(ans)
+        break
+    n -= 3
+    ans += 1
+else:
+    print(-1)
+
+# left = n%5
+# mok = int(n/5)
+# final_mok = int(left/3)
+
+# final = final_mok + mok
+
+# if n%3 != 0 and n%5 != 0:
+#     print(-1)
+# elif (n%5) % 3 != 0:
+#     print(int(n/3))
+# else:
+#     print(final)
+
+
+
+
+# # n은 3과 5로 만들수있어야함
+
