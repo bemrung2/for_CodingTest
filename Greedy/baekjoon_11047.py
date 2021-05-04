42ca0b88cb98ed9a09daf4689995871a75a35a64
bemrung2 <bemrung2@gmail.com>
2021-05-04 22:46:06 +0900

    210504

--- Greedy/baekjoon_11047.py
+++ Greedy/baekjoon_11047.py
@@ -1,5 +1,5 @@
 import sys
-
+ 
 n, k = map(int, sys.stdin.readline().split())
 
 ai = []
@@ -8,11 +8,22 @@ for i in range(n):
 
 ans = 0 
 
-while k>0:
-    if k // ai[n-1] > 0:
+while k > 0:
+    if k > ai[n-1]:
         ans += k // ai[n-1]
         k = k % ai[n-1]
     n -= 1
+
+
+# ans = 0 
+
+# while k>0:
+#     if k // ai[n-1] > 0:
+#         ans += k // ai[n-1]
+#         k = k % ai[n-1]
+#     n -= 1
+
+
 
 
 # for i in range(n-1, -1, -1):
