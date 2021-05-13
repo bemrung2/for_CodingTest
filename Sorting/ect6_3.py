51fb47dce7d2126373b4c25545c576da88ff51ef
bemrung2 <bemrung2@gmail.com>
2021-05-13 19:08:49 +0900

    210513

--- Sorting/ect6_3.py
+++ Sorting/ect6_3.py
@@ -1,10 +1,23 @@
+''' 삽입정렬 '''
+
+# array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+# for i in range(1, len(array)):
+#     for j in range(i, 0, -1): # 인덱스 i부터 1까지 감소하며 반복
+#         if array[j] < array[j-1]:
+#             array[j], array[j-1] = array[j-1], array[j]
+#         else: # 자기보다 작은 데이터 만나면 반복문 탈출
+#             break
+
+# print(array)
+
+
 array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
 for i in range(1, len(array)):
-    for j in range(i, 0, -1): # 인덱스 i부터 1까지 감소하며 반복
+    for j in range(i, 0, -1):
         if array[j] < array[j-1]:
             array[j], array[j-1] = array[j-1], array[j]
-        else: # 자기보다 작은 데이터 만나면 반복문 탈출
+        else:
             break
-
 print(array)
