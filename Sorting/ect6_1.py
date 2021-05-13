1cf796f0c95721ff5e51e91bee9a58aac0c4b515
bemrung2 <bemrung2@gmail.com>
2021-05-13 18:33:04 +0900

    210513

--- Sorting/ect6_1.py
+++ Sorting/ect6_1.py
@@ -1,10 +1,25 @@
+# array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+# # min_index = 0 # 가장 작은 원소의 인덱스
+
+# for i in range(len(array)):
+#     min_index = i # 가장 작은 원소의 인덱스
+#     for j in range(i+1, len(array)):
+#         if array[min_index] > array[j]:
+#             min_index = j
+#     array[i], array[min_index] = array[min_index], array[i] # 스와프
+        
+# print(array)
+
+
+
 array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
 for i in range(len(array)):
-    min_index = i # 가장 작은 원소의 인덱스
+    min_index = i
     for j in range(i+1, len(array)):
         if array[min_index] > array[j]:
             min_index = j
-    array[i], array[min_index] = array[min_index], array[i] # 스와프
-        
+    array[i], array[min_index] = array[min_index], array[i]
+
 print(array)
