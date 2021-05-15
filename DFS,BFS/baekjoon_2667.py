27a689ad790177f2b8b7166c0c132a26dc6969f9
bemrung2 <bemrung2@gmail.com>
2021-05-15 14:44:24 +0900

    210515

--- DFS,BFS/baekjoon_2667.py
+++ DFS,BFS/baekjoon_2667.py
@@ -2,38 +2,80 @@ import sys
 
 n = int(sys.stdin.readline().rstrip())
 
-mat = []
+graph = []
 
 for i in range(n):
-    mat.append(list(map(int, sys.stdin.readline().rstrip())))
+    graph.append(list(map(int, sys.stdin.readline().rstrip())))
 
 cnt = 0 
-apt = []
 
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
 
 def dfs(x,y):
+    graph[x][y] = 0
     global cnt
-    mat[x][y] = 0
     cnt += 1
-    for i in range(4):
+    for i in range(len(dx)):
         nx = x + dx[i]
         ny = y + dy[i]
         if nx < 0 or nx >= n or ny < 0 or ny >= n:
             continue
-        if mat[nx][ny] == 1:
+        if graph[nx][ny] == 1:
             dfs(nx,ny)
+
+lst = []
 
 for i in range(n):
     for j in range(n):
-        if mat[i][j] == 1:
+        if graph[i][j] == 1:
             cnt = 0
-            dfs(i,j)
-            apt.append(cnt)
+            dfs(i, j)
+            lst.append(cnt)
 
-apt.sort()
+lst.sort()
 
-print(len(apt))
-for i in range(len(apt)):
-    print(apt[i])
+print(len(lst))
+for i in range(len(lst)):
+    print(lst[i])
+
+
+# import sys
+
+# n = int(sys.stdin.readline().rstrip())
+
+# mat = []
+
+# for i in range(n):
+#     mat.append(list(map(int, sys.stdin.readline().rstrip())))
+
+# cnt = 0 
+# apt = []
+
+# dx = [-1, 0, 1, 0]
+# dy = [0, -1, 0, 1]
+
+# def dfs(x,y):
+#     global cnt
+#     mat[x][y] = 0
+#     cnt += 1
+#     for i in range(4):
+#         nx = x + dx[i]
+#         ny = y + dy[i]
+#         if nx < 0 or nx >= n or ny < 0 or ny >= n:
+#             continue
+#         if mat[nx][ny] == 1:
+#             dfs(nx,ny)
+
+# for i in range(n):
+#     for j in range(n):
+#         if mat[i][j] == 1: 
+#             cnt = 0
+#             dfs(i,j)
+#             apt.append(cnt)
+
+# apt.sort()
+
+# print(len(apt))
+# for i in range(len(apt)):
+#     print(apt[i])
