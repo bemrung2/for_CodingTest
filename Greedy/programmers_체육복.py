640579d6492aae8b11e655b691a30d698f39b6fd
bemrung2 <bemrung2@gmail.com>
2021-05-21 19:36:25 +0900

    210521

--- Greedy/programmers_체육복.py
+++ Greedy/programmers_체육복.py
@@ -1,12 +1,23 @@
-
-def solution(n, lost, reserve):
-    reser_del = set(reserve)-set(lost)
-    lost_del = set(lost)-set(reserve)
+def solution(n, lost, reverse):
+    reverse_del = set(reverse) - set(lost)
+    lost_del = set(lost) - set(reverse)
     
-    for i in reser_del:
+    for i in reverse_del:
         if i-1 in lost_del:
             lost_del.remove(i-1)
         elif i+1 in lost_del:
             lost_del.remove(i+1)
     
-    return n-len(lost_del)
+    return n - len(lost_del)
+
+# def solution(n, lost, reverse):
+#     reverse_del = set(reverse) - set(lost)
+#     lost_del = set(lost) - set(reverse)
+
+#     for i in reverse_del:
+#         if i-1 in lost_del:
+#             lost_del.remove(i-1)
+#         elif i+1 in lost_del:
+#             lost_del.remove(i+1)
+
+#     return n-len(lost_del)
